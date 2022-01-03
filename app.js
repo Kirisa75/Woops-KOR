960175dbd250c587cc007ba6fc7e1e6ec1c75280
Kirisa75 <68466305+Kirisa75@users.noreply.github.com>
2022-01-03 21:31:44 +0900

    delete express

--- app.js
+++ app.js
@@ -13,20 +13,6 @@ const queue = new Map()
 /////////////////////
 
 
-const express = require('express');
-const server = express();
-
-server.all('/', (req, res)=>{
-    res.send('Your bot is alive!')
-})
-
-function keepAlive(){
-    server.listen(3000, ()=>{console.log("Server is Ready!" + Date.now()) });
-}
-
-keepAlive();
-
-
 /////////////////////
 
 client.on('ready', () => {
